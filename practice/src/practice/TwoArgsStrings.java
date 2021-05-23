487e0b291967854f0ec38e9d14e6181b9e4912a5
Jyothsna <Jyothsna@DESKTOP-AC0DFEC>
2021-05-23 23:22:51 +0530

    Practice till arrays, Objects,Conditional Statements,loops

---
+++ practice/src/practice/TwoArgsStrings.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class TwoArgsStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[0]+","+args[1]);
+
+	}
+
+}
